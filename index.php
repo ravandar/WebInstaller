e422078900a43b1db2336228e68168fe6ca83211
Ravandar <awli.khaleghi+ravandar@gmail.com>
2024-08-15 00:37:13 +0330

    Initial

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <title>Ravandar Setup</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <link rel='stylesheet' type='text/css' media='screen' href='resources/ui.css'>
+        <script src='resources/ui.js'></script>
+    </head>
+    <body>
+        <div id="installer">
+            <div class="header">Install</div>
+            <div class="content">
+                Yes
+            </div>
+        </div>
+        <div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+        </div>
+
+    </body>
+</html>
+<?php
+
+class Installer
+{
+    
+}
